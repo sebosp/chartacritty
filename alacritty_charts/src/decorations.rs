e533c4bdebb26263664cd6e085de69b8bfca48ca
Seb Ospina <kraige@gmail.com>
2019-12-13 10:26:00 +0100

    Moving decorations to its own file

---
+++ alacritty_charts/src/decorations.rs
@@ -0,0 +1,411 @@
+//! Alacritty Chart Decorations are drawings or effects over drawings that
+//! are not tied to metrics, these could be reference points or alarms/etc.
+
+/// `Decoration` contains several types of decorations to add to a chart
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[serde(tag = "type")]
+pub enum Decoration {
+    #[serde(rename = "reference")]
+    Reference(ReferencePointDecoration),
+    #[serde(rename = "alert")]
+    Alert(ActiveAlertUnderLineDecoration),
+    None,
+    /* Maybe add Average, threshold coloring (turn line red after a certain
+     * point) */
+}
+
+impl Default for Decoration {
+    fn default() -> Decoration {
+        Decoration::None
+    }
+}
+
+// XXX: Maybe this should turn into a trait
+impl Decoration {
+    /// `width` of the Decoration as it may need space to be drawn, otherwise
+    /// the decoration and the data itself would overlap, these are pixels
+    fn width(&self) -> f32 {
+        match self {
+            Decoration::Reference(d) => d.padding.x * 2., // it needs space left and right
+            Decoration::Alert(_d) => 0f32, // The alert is drawn below the chart data.
+            Decoration::None => 0f32,
+        }
+    }
+
+    /// `top_value` is the Y value of the decoration, it needs to be
+    /// in the range of the metrics that have been collected, thus f64
+    /// this is the highest point the Decoration will use
+    fn top_value(&self) -> f64 {
+        match self {
+            Decoration::Reference(ref d) => d.top_value(),
+            Decoration::Alert => 0f64,
+            Decoration::None => 0f64,
+        }
+    }
+
+    /// `bottom_value` is the Y value of the decoration, it needs to be
+    /// in the range of the metrics that have been collected, thus f64
+    /// this is the lowest point the Decoration will use
+    fn bottom_value(&self) -> f64 {
+        match self {
+            Decoration::Reference(d) => d.value - d.value * d.height_multiplier,
+            Decoration::Alert(_d) => 0f64,
+            Decoration::None => 0f64,
+        }
+    }
+
+    /// `update_opengl_vecs` calls the decoration update methods
+    fn update_opengl_vecs(
+        &mut self,
+        display_size: SizeInfo,
+        offset: Value2D,
+        chart_max_value: f64,
+    ) {
+        match self {
+            Decoration::Reference(ref mut d) => {
+                d.update_opengl_vecs(display_size, offset, chart_max_value)
+            },
+            Decoration::Alert(ref mut d) => {
+                d.update_opengl_vecs(display_size, offset, chart_max_value)
+            },
+            Decoration::None => (),
+        }
+    }
+
+    /// `opengl_vertices` returns the representation of the decoration in
+    /// opengl. These are for now GL_LINES and 2D
+    pub fn opengl_vertices(&self) -> Vec<f32> {
+        match self {
+            Decoration::Reference(d) => d.opengl_vertices(),
+            Decoration::Alert(d) => d.opengl_vertices(),
+            Decoration::None => vec![],
+        }
+    }
+
+    /// `color` returns the Rgb for the decoration
+    pub fn color(&self) -> Rgb {
+        match self {
+            Decoration::Reference(d) => d.color,
+            Decoration::Alert(d) => d.color,
+            Decoration::None => Rgb::default(),
+        }
+    }
+
+    /// `alpha` returns the transparency for the decoration
+    pub fn alpha(&self) -> f32 {
+        match self {
+            Decoration::Reference(d) => d.alpha,
+            Decoration::Alert(d) => d.alpha,
+            Decoration::None => 0.0f32,
+        }
+    }
+}
+/// `ReferencePointDecoration` draws a fixed point to give a reference point
+/// of what a drawn value may mean
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct ReferencePointDecoration {
+    /// The value at which to draw the reference point
+    pub value: f64,
+
+    /// The reference point will use additional height for the axis line
+    /// this makes it fit in the configured space, basically the value
+    /// will be incremented by this additional percentage to give more
+    /// space to draw the axis tick
+    #[serde(default)]
+    pub height_multiplier: f64,
+
+    /// RGB color
+    #[serde(default)]
+    pub color: Rgb,
+
+    /// Transparency
+    #[serde(default)]
+    pub alpha: f32,
+
+    /// The pixels to separate from the left and right
+    #[serde(default)]
+    pub padding: Value2D,
+
+    /// The opengl vertices is stored in this vector
+    /// The capacity is always 12, see opengl_vertices()
+    #[serde(default)]
+    pub opengl_data: Vec<f32>,
+}
+
+impl Default for ReferencePointDecoration {
+    fn default() -> ReferencePointDecoration {
+        ReferencePointDecoration {
+            value: 1.0,
+            height_multiplier: 0.05,
+            color: Rgb::default(),
+            alpha: 0.5,
+            padding: Value2D {
+                x: 1f32,
+                y: 0f32, // No top/bottom padding
+            },
+            opengl_data: vec![],
+        }
+    }
+}
+
+impl ReferencePointDecoration {
+    /// `opengl_vertices` Scales the Marker Line to the current size of
+    /// the displayed points
+    pub fn opengl_vertices(&self) -> Vec<f32> {
+        self.opengl_data.clone()
+    }
+
+    /// `top_value` increments the reference point value by an additional
+    /// percentage to account for space to draw the axis tick
+    pub fn top_value(&self) -> f64 {
+        self.value + self.value * self.height_multiplier
+    }
+
+    /// `bottom_value` decrements the reference point value by a percentage
+    /// to account for space to draw the axis tick
+    pub fn bottom_value(&self) -> f64 {
+        self.value - self.value * self.height_multiplier
+    }
+
+    /// `update_opengl_vecs` Draws a marker at a fixed position for
+    /// reference.
+    pub fn update_opengl_vecs(
+        &mut self,
+        display_size: SizeInfo,
+        offset: Value2D,
+        chart_max_value: f64,
+    ) {
+        debug!("ReferencePointDecoration:update_opengl_vecs: Starting");
+        if 12 != self.opengl_data.capacity() {
+            self.opengl_data = vec![0.; 12];
+        }
+        // The vertexes of the above marker idea can be represented as
+        // connecting lines for these coordinates:
+        //         |Actual Draw Metric Data|
+        // x1,y2   |                       |   x2,y2
+        // x1,y1 --|-----------------------|-- x2,y1
+        // x1,y3   |                       |   x2,y3
+        // |- 10% -|-         80%         -|- 10% -|
+        // TODO: Add marker_line color to opengl
+        // TODO: Call only when max or min have changed in collected metrics
+        //
+        // Calculate X coordinates:
+        let x1 = display_size.scale_x(offset.x);
+        let x2 = display_size.scale_x(offset.x + display_size.chart_width);
+
+        // Calculate Y, the marker hints are 10% of the current values
+        // This means that the
+        let y1 = display_size.scale_y(chart_max_value, self.value);
+        let y2 = display_size.scale_y(chart_max_value, self.top_value());
+        let y3 = display_size.scale_y(chart_max_value, self.bottom_value());
+
+        // Build the left most axis "tick" mark.
+        self.opengl_data[0] = x1;
+        self.opengl_data[1] = y2;
+        self.opengl_data[2] = x1;
+        self.opengl_data[3] = y3;
+
+        // Create the line to the other side
+        self.opengl_data[4] = x1;
+        self.opengl_data[5] = y1;
+        self.opengl_data[6] = x2;
+        self.opengl_data[7] = y1;
+        // Finish the axis "tick" on the other side
+        self.opengl_data[8] = x2;
+        self.opengl_data[9] = y3;
+        self.opengl_data[10] = x2;
+        self.opengl_data[11] = y2;
+        debug!(
+            "ReferencePointDecoration:update_opengl_vecs: Finished: {:?}",
+            self.opengl_data
+        );
+    }
+}
+
+
+/// `ActiveAlertUnderLineDecoration` draws an underlined series of
+/// red triangles below a portion of the screen to denote alert below a
+/// chart
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct ActiveAlertUnderLineDecoration {
+    /// The value at which to draw the reference point
+    pub value: f64,
+
+    #[serde(default)]
+    pub color: Rgb,
+
+    /// Transparency
+    #[serde(default)]
+    pub alpha: f32,
+
+    /// The pixels to separate from the left and right
+    #[serde(default)]
+    pub padding: Value2D,
+
+    /// The opengl vertices is stored in this vector
+    /// The capacity is dynamic, it draws a triangle every n pixels, see
+    /// opengl_vertices()
+    #[serde(default)]
+    pub opengl_data: Vec<f32>,
+}
+
+impl Default for ActiveAlertUnderLineDecoration {
+    fn default() -> ActiveAlertUnderLineDecoration {
+        ActiveAlertUnderLineDecoration {
+            value: 1.0,
+            color: Rgb::default(),
+            alpha: 0.5,
+            padding: Value2D {
+                x: 0f32,
+                y: 1f32, // XXX: figure out how to reserve space vertically
+            },
+            opengl_data: vec![],
+        }
+    }
+}
+
+impl ActiveAlertUnderLineDecoration {
+    /// `opengl_vertices` Scales the Marker Line to the current size of
+    /// the displayed points
+    pub fn opengl_vertices(&self) -> Vec<f32> {
+        self.opengl_data.clone()
+    }
+
+    /// `top_value` increments the reference point value by an additional
+    /// percentage to account for space to draw the axis tick
+    pub fn top_value(&self) -> f64 {
+        self.value + self.value * self.height_multiplier
+    }
+
+    /// `bottom_value` decrements the reference point value by a percentage
+    /// to account for space to draw the axis tick
+    pub fn bottom_value(&self) -> f64 {
+        self.value - self.value * self.height_multiplier
+    }
+
+    /// `update_opengl_vecs` Draws a marker at a fixed position for
+    /// reference.
+    pub fn update_opengl_vecs(
+        &mut self,
+        display_size: SizeInfo,
+        offset: Value2D,
+        chart_max_value: f64,
+    ) {
+        debug!("ActiveAlertUnderLineDecoration:update_opengl_vecs: Starting");
+        if 12 != self.opengl_data.capacity() {
+            self.opengl_data = vec![0.; 12];
+        }
+        // The vertexes of the above marker idea can be represented as
+        // connecting lines for these coordinates:
+        //         |Actual Draw Metric Data|
+        // x1,y2   |                       |   x2,y2
+        // x1,y1 --|-----------------------|-- x2,y1
+        // x1,y3   |                       |   x2,y3
+        // |- 10% -|-         80%         -|- 10% -|
+        // TODO: Add marker_line color to opengl
+        // TODO: Call only when max or min have changed in collected metrics
+        //
+        // Calculate X coordinates:
+        let x1 = display_size.scale_x(offset.x);
+        let x2 = display_size.scale_x(offset.x + display_size.chart_width);
+
+        // Calculate Y, the marker hints are 10% of the current values
+        // This means that the
+        let y1 = display_size.scale_y(chart_max_value, self.value);
+        let y2 = display_size.scale_y(chart_max_value, self.top_value());
+        let y3 = display_size.scale_y(chart_max_value, self.bottom_value());
+
+        // Build the left most axis "tick" mark.
+        self.opengl_data[0] = x1;
+        self.opengl_data[1] = y2;
+        self.opengl_data[2] = x1;
+        self.opengl_data[3] = y3;
+
+        // Create the line to the other side
+        self.opengl_data[4] = x1;
+        self.opengl_data[5] = y1;
+        self.opengl_data[6] = x2;
+        self.opengl_data[7] = y1;
+        // Finish the axis "tick" on the other side
+        self.opengl_data[8] = x2;
+        self.opengl_data[9] = y3;
+        self.opengl_data[10] = x2;
+        self.opengl_data[11] = y2;
+        debug!(
+            "ActiveAlertUnderLineDecoration:update_opengl_vecs: Finished: {:?}",
+            self.opengl_data
+        );
+    }
+}
+
+// XXX: Maybe this should turn into a trait
+impl Decoration {
+    /// `width` of the Decoration as it may need space to be drawn, otherwise
+    /// the decoration and the data itself would overlap, these are pixels
+    fn width(&self) -> f32 {
+        match self {
+            Decoration::Reference(d) => d.padding.x * 2., // it needs space left and right
+            Decoration::None => 0f32,
+        }
+    }
+
+    /// `top_value` is the Y value of the decoration, it needs to be
+    /// in the range of the metrics that have been collected, thus f64
+    /// this is the highest point the Decoration will use
+    fn top_value(&self) -> f64 {
+        match self {
+            Decoration::Reference(ref d) => d.top_value(),
+            Decoration::None => 0f64,
+        }
+    }
+
+    /// `bottom_value` is the Y value of the decoration, it needs to be
+    /// in the range of the metrics that have been collected, thus f64
+    /// this is the lowest point the Decoration will use
+    fn bottom_value(&self) -> f64 {
+        match self {
+            Decoration::Reference(d) => d.value - d.value * d.height_multiplier,
+            Decoration::None => 0f64,
+        }
+    }
+
+    /// `update_opengl_vecs` calls the decoration update methods
+    fn update_opengl_vecs(
+        &mut self,
+        display_size: SizeInfo,
+        offset: Value2D,
+        chart_max_value: f64,
+    ) {
+        match self {
+            Decoration::Reference(ref mut d) => {
+                d.update_opengl_vecs(display_size, offset, chart_max_value)
+            }
+            Decoration::None => (),
+        }
+    }
+
+    /// `opengl_vertices` returns the representation of the decoration in
+    /// opengl. These are for now GL_LINES and 2D
+    pub fn opengl_vertices(&self) -> Vec<f32> {
+        match self {
+            Decoration::Reference(d) => d.opengl_vertices(),
+            Decoration::None => vec![],
+        }
+    }
+
+    /// `color` returns the Rgb for the decoration
+    pub fn color(&self) -> Rgb {
+        match self {
+            Decoration::Reference(d) => d.color,
+            Decoration::None => Rgb::default(),
+        }
+    }
+
+    /// `alpha` returns the transparency for the decoration
+    pub fn alpha(&self) -> f32 {
+        match self {
+            Decoration::Reference(d) => d.alpha,
+            Decoration::None => 0.0f32,
+        }
+    }
+}
