d1488da69bfb63c353f52fcfb74dd8846d8afed9
Seb Ospina <kraige@gmail.com>
2020-07-11 20:26:16 +0200

    Undoing moving create_shader to alacritty_common due to build setup

--- alacritty/src/renderer/mod.rs
+++ alacritty/src/renderer/mod.rs
@@ -20,7 +20,6 @@ use crate::gl;
 use crate::gl::types::*;
 use crate::renderer::rects::RenderRect;
 use alacritty_common::index::{Column, Line};
-use alacritty_common::renderer::create_shader;
 use alacritty_terminal::config::{self, Config, Delta, Font, StartupMode};
 use alacritty_terminal::term::cell::{self, Flags};
 use alacritty_terminal::term::color::Rgb;
@@ -1495,6 +1494,48 @@ fn create_program(vertex: GLuint, fragment: GLuint) -> Result<GLuint, ShaderCrea
         } else {
             Err(ShaderCreationError::Link(get_program_info_log(program)))
         }
+    }
+}
+
+fn create_shader(
+    path: &str,
+    kind: GLenum,
+    source: Option<&'static str>,
+) -> Result<GLuint, ShaderCreationError> {
+    let from_disk;
+    let source = if let Some(src) = source {
+        src
+    } else {
+        from_disk = fs::read_to_string(path)?;
+        &from_disk[..]
+    };
+
+    let len: [GLint; 1] = [source.len() as GLint];
+
+    let shader = unsafe {
+        let shader = gl::CreateShader(kind);
+        gl::ShaderSource(shader, 1, &(source.as_ptr() as *const _), len.as_ptr());
+        gl::CompileShader(shader);
+        shader
+    };
+
+    let mut success: GLint = 0;
+    unsafe {
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut success);
+    }
+
+    if success == GLint::from(gl::TRUE) {
+        Ok(shader)
+    } else {
+        // Read log.
+        let log = get_shader_info_log(shader);
+
+        // Cleanup.
+        unsafe {
+            gl::DeleteShader(shader);
+        }
+
+        Err(ShaderCreationError::Compile(PathBuf::from(path), log))
     }
 }
 
