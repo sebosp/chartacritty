caf09945cae8ec4957a7cd53031d2a9ea4b28daa
Seb Ospina <kraige@gmail.com>
2020-08-22 22:00:54 +0200

    color should be 4 items

--- res/hex_bg.v.glsl
+++ res/hex_bg.v.glsl
@@ -13,11 +13,11 @@
 // limitations under the License.
 #version 330 core
 layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec3 vertexColor;
+layout (location = 1) in vec4 vertexColor;
 
 uniform float epoch_millis;
 
-out vec3 fragmentColor;
+out vec4 fragmentColor;
 
 void main()
 {
