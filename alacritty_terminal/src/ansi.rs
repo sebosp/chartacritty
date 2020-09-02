b0d0c5ddd63538b97ba664ba550f02770fba251c
Seb Ospina <kraige@gmail.com>
2020-09-02 19:38:23 +0200

    Moved NamedColor and Color to alacritty_common

--- alacritty_terminal/src/ansi.rs
+++ alacritty_terminal/src/ansi.rs
@@ -8,6 +8,7 @@ use serde::{Deserialize, Serialize};
 use vte::{Params, ParamsIter};
 
 use crate::term::color::Rgb;
+pub use alacritty_common::ansi::Color;
 use alacritty_common::ansi::{CharsetIndex, StandardCharset};
 use alacritty_common::index::{Column, Line};
 
@@ -487,129 +488,6 @@ pub enum TabulationClearMode {
     Current,
     /// Clear all stops.
     All,
-}
-
-/// Standard colors.
-///
-/// The order here matters since the enum should be castable to a `usize` for
-/// indexing a color list.
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Serialize, Deserialize)]
-pub enum NamedColor {
-    /// Black.
-    Black = 0,
-    /// Red.
-    Red,
-    /// Green.
-    Green,
-    /// Yellow.
-    Yellow,
-    /// Blue.
-    Blue,
-    /// Magenta.
-    Magenta,
-    /// Cyan.
-    Cyan,
-    /// White.
-    White,
-    /// Bright black.
-    BrightBlack,
-    /// Bright red.
-    BrightRed,
-    /// Bright green.
-    BrightGreen,
-    /// Bright yellow.
-    BrightYellow,
-    /// Bright blue.
-    BrightBlue,
-    /// Bright magenta.
-    BrightMagenta,
-    /// Bright cyan.
-    BrightCyan,
-    /// Bright white.
-    BrightWhite,
-    /// The foreground color.
-    Foreground = 256,
-    /// The background color.
-    Background,
-    /// Color for the cursor itself.
-    Cursor,
-    /// Dim black.
-    DimBlack,
-    /// Dim red.
-    DimRed,
-    /// Dim green.
-    DimGreen,
-    /// Dim yellow.
-    DimYellow,
-    /// Dim blue.
-    DimBlue,
-    /// Dim magenta.
-    DimMagenta,
-    /// Dim cyan.
-    DimCyan,
-    /// Dim white.
-    DimWhite,
-    /// The bright foreground color.
-    BrightForeground,
-    /// Dim foreground.
-    DimForeground,
-}
-
-impl NamedColor {
-    pub fn to_bright(self) -> Self {
-        match self {
-            NamedColor::Foreground => NamedColor::BrightForeground,
-            NamedColor::Black => NamedColor::BrightBlack,
-            NamedColor::Red => NamedColor::BrightRed,
-            NamedColor::Green => NamedColor::BrightGreen,
-            NamedColor::Yellow => NamedColor::BrightYellow,
-            NamedColor::Blue => NamedColor::BrightBlue,
-            NamedColor::Magenta => NamedColor::BrightMagenta,
-            NamedColor::Cyan => NamedColor::BrightCyan,
-            NamedColor::White => NamedColor::BrightWhite,
-            NamedColor::DimForeground => NamedColor::Foreground,
-            NamedColor::DimBlack => NamedColor::Black,
-            NamedColor::DimRed => NamedColor::Red,
-            NamedColor::DimGreen => NamedColor::Green,
-            NamedColor::DimYellow => NamedColor::Yellow,
-            NamedColor::DimBlue => NamedColor::Blue,
-            NamedColor::DimMagenta => NamedColor::Magenta,
-            NamedColor::DimCyan => NamedColor::Cyan,
-            NamedColor::DimWhite => NamedColor::White,
-            val => val,
-        }
-    }
-
-    pub fn to_dim(self) -> Self {
-        match self {
-            NamedColor::Black => NamedColor::DimBlack,
-            NamedColor::Red => NamedColor::DimRed,
-            NamedColor::Green => NamedColor::DimGreen,
-            NamedColor::Yellow => NamedColor::DimYellow,
-            NamedColor::Blue => NamedColor::DimBlue,
-            NamedColor::Magenta => NamedColor::DimMagenta,
-            NamedColor::Cyan => NamedColor::DimCyan,
-            NamedColor::White => NamedColor::DimWhite,
-            NamedColor::Foreground => NamedColor::DimForeground,
-            NamedColor::BrightBlack => NamedColor::Black,
-            NamedColor::BrightRed => NamedColor::Red,
-            NamedColor::BrightGreen => NamedColor::Green,
-            NamedColor::BrightYellow => NamedColor::Yellow,
-            NamedColor::BrightBlue => NamedColor::Blue,
-            NamedColor::BrightMagenta => NamedColor::Magenta,
-            NamedColor::BrightCyan => NamedColor::Cyan,
-            NamedColor::BrightWhite => NamedColor::White,
-            NamedColor::BrightForeground => NamedColor::Foreground,
-            val => val,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-pub enum Color {
-    Named(NamedColor),
-    Spec(Rgb),
-    Indexed(u8),
 }
 
 /// Terminal character attributes.
