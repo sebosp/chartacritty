305020e85d34969c0fd2dbf138e1462bf48a2d28
Seb Ospina <sebastian.ospina@irdeto.com>
2022-11-04 13:30:39 +0100

    clippy lints

--- alacritty_terminal/src/async_utils/mod.rs
+++ alacritty_terminal/src/async_utils/mod.rs
@@ -435,10 +435,8 @@ pub fn spawn_charts_intervals(
     charts_tx: mpsc::Sender<AsyncTask>,
     tokio_handle: tokio::runtime::Handle,
 ) {
-    let mut chart_index = 0usize;
-    for chart in charts {
-        let mut series_index = 0usize;
-        for series in chart.sources {
+    for (chart_index, chart) in charts.into_iter().enumerate() {
+        for (series_index, series) in chart.sources.into_iter().enumerate() {
             if let TimeSeriesSource::PrometheusTimeSeries(ref prom) = series {
                 event!(
                     Level::DEBUG,
@@ -468,9 +466,7 @@ pub fn spawn_charts_intervals(
                     );
                 });
             }
-            series_index += 1;
         }
-        chart_index += 1;
     }
 }
 /// `spawn_datasource_interval_polls` creates intervals for each series requested
@@ -603,7 +599,7 @@ where
 {
     // let decor_config = config.decorations.clone();
     let chart_config = chart_config.clone();
-    let tokio_thread = ::std::thread::Builder::new()
+    ::std::thread::Builder::new()
         .name("async I/O".to_owned())
         .spawn(move || {
             let mut tokio_runtime =
@@ -625,8 +621,7 @@ where
             });
             info!("Tokio runtime finished.");
         })
-        .expect("Unable to start async I/O thread");
-    tokio_thread
+        .expect("Unable to start async I/O thread")
 }
 
 /// `run` is an example use of the crate without drawing the data.
