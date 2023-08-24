8f7e4b7b5b22ed9e20acb20900f06935499e797c
Seb Ospina <kraige@gmail.com>
2023-08-24 18:31:27 +0200

    Removing lots of things not needed moving to lyon
    Signed-off-by: Seb Ospina <kraige@gmail.com>

--- alacritty_terminal/src/decorations/nannou.rs
+++ alacritty_terminal/src/decorations/nannou.rs
@@ -1,60 +1,16 @@
-//! Nannou-based decorations for Alacritty
+//! Lyon-based decorations for Alacritty
 
 use super::PolarClockState;
-use crate::term::color::Rgb;
 use crate::term::SizeInfo;
 use chrono::prelude::*;
-use lyon::tessellation::{FillTessellator, StrokeTessellator};
-use nannou::draw;
-pub use nannou::draw::primitive::Primitive;
-use nannou::draw::renderer::{GlyphCache, RenderPrimitive};
-pub use nannou::draw::State;
-use nannou::glam::Vec2;
+use lyon::math::Point;
+use lyon::tessellation::VertexBuffers;
+use palette::rgb::{Rgb, Rgba};
 use serde::{Deserialize, Serialize};
 use super::moon_phase::MoonPhaseState;
 
-#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
-pub enum NannouDrawArrayMode {
-    Points,
-    LineStrip,
-    LineLoop,
-    GlTriangles,
-}
-
-impl Default for NannouDrawArrayMode {
-    fn default() -> Self {
-        Self::LineStrip
-    }
-}
-
-impl From<nannou::draw::primitive::Primitive> for NannouDrawArrayMode {
-    fn from(src: nannou::draw::primitive::Primitive) -> Self {
-        match src {
-            nannou::draw::primitive::Primitive::Mesh(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Tri(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Polygon(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Ellipse(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Quad(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Rect(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Line(_) => NannouDrawArrayMode::LineStrip,
-            nannou::draw::primitive::Primitive::Text(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Texture(_) => NannouDrawArrayMode::GlTriangles,
-            nannou::draw::primitive::Primitive::Path(_) => NannouDrawArrayMode::LineStrip,
-            _ => NannouDrawArrayMode::LineStrip,
-        }
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
-pub struct NannouVertices {
-    #[serde(default)]
-    pub draw_array_mode: NannouDrawArrayMode,
-    #[serde(default)]
-    pub vecs: Vec<f32>,
-}
-
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
-pub struct NannouDecoration {
+pub struct LyonDecoration {
     pub color: Rgb,
     pub alpha: f32,
     #[serde(default)]
@@ -65,7 +21,7 @@ pub struct NannouDecoration {
     #[serde(default)]
     pub moon_state: MoonPhaseState,
     #[serde(default)]
-    pub vertices: Vec<NannouVertices>,
+    pub vertices: Vec<f32>,
     #[serde(default = "local_now")]
     pub now: DateTime<Local>,
     #[serde(default)]
@@ -81,23 +37,7 @@ fn local_now() -> DateTime<Local> {
     Local::now()
 }
 
-// TODO: Move this somewhere sensical...
-fn new_glyph_cache() -> GlyphCache {
-    let size = nannou::draw::Renderer::DEFAULT_GLYPH_CACHE_SIZE;
-    let scale_tolerance = nannou::draw::Renderer::DEFAULT_GLYPH_CACHE_SCALE_TOLERANCE;
-    let position_tolerance = nannou::draw::Renderer::DEFAULT_GLYPH_CACHE_POSITION_TOLERANCE;
-    let [w, h] = size;
-    let cache = nannou::text::GlyphCache::builder()
-        .dimensions(w, h)
-        .scale_tolerance(scale_tolerance)
-        .position_tolerance(position_tolerance)
-        .build();
-    let pixel_buffer = vec![0u8; w as usize * h as usize];
-    let requires_upload = false;
-    GlyphCache { cache, pixel_buffer, requires_upload }
-}
-
-impl NannouDecoration {
+impl LyonDecoration {
     pub fn new(color: Rgb, alpha: f32, size_info: SizeInfo, radius: f32) -> Self {
         let coords = super::gen_hex_grid_positions(size_info, radius);
         let center_idx = super::find_hexagon_grid_center_idx(&coords, size_info, radius);
@@ -146,77 +86,36 @@ impl NannouDecoration {
         self.update_opengl_vecs();
     }
 
-    /// Called after instantiation of the NannouDecoration, it will initialize the vertices for the
+    /// Called after instantiation of the LyonDecoration, it will initialize the vertices for the
     /// decorations.
     pub fn update_opengl_vecs(&mut self) {
-        // tracing::info!("NannouDecoration::update_opengl_vecs(size_info) {:?}, center_idx: {}, x: {}, y:{}, radius: {}, coords: {:?}", self.size_info, center_idx, coord.x, coord.y, self.radius, coords);
+        // tracing::info!("LyonDecoration::update_opengl_vecs(size_info) {:?}, center_idx: {}, x: {}, y:{}, radius: {}, coords: {:?}", self.size_info, center_idx, coord.x, coord.y, self.radius, coords);
         self.vertices = self.gen_vertices();
     }
 
     // Transforms nannou::draw::Draw into xyrgba vertices we can draw through our renderer
     pub fn gen_vertices_from_nannou_draw(
-        draw: draw::Draw,
+        geometry: VertexBuffers<Point, u16>,
         size_info: SizeInfo,
-    ) -> Vec<NannouVertices> {
-        let mut res = vec![];
-        draw.finish_remaining_drawings();
-        // Trying to adapt nannou crate nannou/src/draw/renderer/mod.rs `fill()` function
-        let mut mesh = draw::Mesh::default();
-        // Construct the glyph cache.
-        let mut glyph_cache = new_glyph_cache();
-        let mut fill_tessellator = FillTessellator::new();
-        let mut stroke_tessellator = StrokeTessellator::new();
-        // Keep track of context changes.
-        let mut curr_ctxt = draw::Context::default();
-        let draw_cmds: Vec<_> = draw.drain_commands().collect();
-        let draw_state = draw.state();
-        let intermediary_state = draw_state.intermediary_state();
-        let scale_factor = 1.;
-        for cmd in draw_cmds {
-            match cmd {
-                draw::DrawCommand::Context(ctxt) => curr_ctxt = ctxt,
-                draw::DrawCommand::Primitive(prim) => {
-                    // Info required during rendering.
-                    let ctxt = draw::renderer::RenderContext {
-                        intermediary_mesh: intermediary_state.intermediary_mesh(),
-                        path_event_buffer: intermediary_state.path_event_buffer(),
-                        path_points_colored_buffer: intermediary_state
-                            .path_points_colored_buffer(),
-                        path_points_textured_buffer: intermediary_state
-                            .path_points_textured_buffer(),
-                        text_buffer: intermediary_state.text_buffer(),
-                        theme: draw_state.theme(),
-                        transform: &curr_ctxt.transform,
-                        fill_tessellator: &mut fill_tessellator,
-                        stroke_tessellator: &mut stroke_tessellator,
-                        glyph_cache: &mut glyph_cache,
-                        output_attachment_size: Vec2::new(2., 2.),
-                        output_attachment_scale_factor: scale_factor,
-                    };
-
-                    let draw_array_mode = prim.clone().into();
-                    // Render the primitive.
-                    let _render = prim.render_primitive(ctxt, &mut mesh);
-                    let mut primitive_render_vecs = vec![];
-                    for vx in mesh.vertices() {
-                        primitive_render_vecs.push(size_info.scale_x(vx.x));
-                        primitive_render_vecs.push(size_info.scale_y(vx.y));
-                        primitive_render_vecs.push(0.0);
-                        primitive_render_vecs.push(vx.color.red);
-                        primitive_render_vecs.push(vx.color.green);
-                        primitive_render_vecs.push(vx.color.blue);
-                        primitive_render_vecs.push(vx.color.alpha);
-                    }
-                    res.push(NannouVertices { draw_array_mode, vecs: primitive_render_vecs });
-                },
-            }
-        }
-        res
+        color: Rgba<f32>,
+    ) -> Vec<f32> {
+    // No idea how gl Draw Elements work so let's build the payload by hand:
+    let mut vertices: Vec<f32> = Vec::with_capacity(geometry.indices.len() * 7usize);
+    for idx in geometry.indices {
+        vertices.push(geometry.vertices[idx as usize].x);
+        vertices.push(geometry.vertices[idx as usize].y);
+        vertices.push(0.0); // z
+        vertices.push(color.color.red);
+        vertices.push(color.color.green);
+        vertices.push(color.color.blue);
+        vertices.push(color.alpha);
+    }
+    vertices
     }
 
     /// `gen_vertices` Returns the vertices for an tree created at center x,y with a
     /// specific radius
-    pub fn gen_vertices(&self) -> Vec<NannouVertices> {
+    pub fn gen_vertices(&self) -> Vec<f32> {
         /*let x = self.x;
         let y = self.y;
         let x_60_degrees_offset = COS_60 * self.radius;
